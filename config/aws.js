a7a78b9f5621bba2de287aa6d5bfd702f47f62d6
sinrim10 <sinrim10@naver.com>
2016-09-26 23:55:55 +0900

    Update aws.js

--- config/aws.js
+++ config/aws.js
@@ -3,6 +3,6 @@
  */
 module.exports ={
     region : 'ap-northeast-2',
-    accessKeyId : 'AKIAJT7BOR7VPCRRVX4Q',
-    secretAccessKey : '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'
+    accessKeyId : '',
+    secretAccessKey : ''
 }
